a302b3bddc74631194fd360e452f25f748766c80
Siremmy9 <siremmy9@gmail.com>
2026-09-09 07:20:07 +0100

    Whatsapp redirected to CEO's Line

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@
 const STUDIO_CONFIG = {
   name: "Angel's Treat",
   phone: "+2348141713798",
-  whatsapp: "2348141713798", // digits only, no + , used for wa.me links
+  whatsapp: "2347088310321", // digits only, no + , used for wa.me links
   email: "hello@angelstreat.com",
   address: "23 Seriki Balogun street, Bashorun Estate, Majek",
   instagram: "https://instagram.com/angelstreat",
